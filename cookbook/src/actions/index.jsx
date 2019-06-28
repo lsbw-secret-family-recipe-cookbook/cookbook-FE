3f466b75595f3ddfa3f83012e0711f2b7f8895df
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-06-28 00:47:32 -0400

    Successful recipe edit now redirects to individual recipe view.

--- cookbook/src/actions/index.jsx
+++ cookbook/src/actions/index.jsx
@@ -90,12 +90,14 @@ export const UPDATE_RECIPE_START = "EDIT_RECIPE_START";
 export const UPDATE_RECIPE_SUCCESS = "EDIT_RECIPE_SUCCESS";
 export const UPDATE_RECIPE_FAILURE = "EDIT_RECIPE_FAILURE";
 
-export const updateRecipe = (recipeID, updatedRecipe) => dispatch => {
+export const updateRecipe = (recipeID, updatedRecipe, history) => dispatch => {
   dispatch({ type: UPDATE_RECIPE_START });
   axiosWithAuth()
     .put(`/recipes/${recipeID}`, updatedRecipe)
     .then(res => {
       dispatch({ type: UPDATE_RECIPE_SUCCESS, payload: res.data });
+      const recipe_id = res.data.id
+      history.push(`/recipes/view/${recipe_id}`)
     })
     .catch(err => {
       dispatch({ type: UPDATE_RECIPE_FAILURE, payload: err });
